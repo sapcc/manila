0b3be0f5255be70edfac55e2e2638c022d6c9d39
Maurice Schreiber <maurice.schreiber@sap.com>
2017-06-08 13:39:30 +0200

    use security service ou
    sets organizational unit in AD at NetApp C-mode CIFS server
    
    Change-Id: I8edc587c76b3eb18dce0c0564167c95c1ebfccf4
    Closes-Bug: #1696669

--- manila/share/drivers/netapp/dataontap/client/client_cmode.py
+++ manila/share/drivers/netapp/dataontap/client/client_cmode.py
@@ -1393,6 +1393,7 @@ class NetAppCmodeClient(client_base.NetAppBaseClient):
             'admin-password': security_service['password'],
             'force-account-overwrite': 'true',
             'cifs-server': cifs_server,
+            'organizational-unit': security_service['ou'],
             'domain': security_service['domain'],
         }
         try:
