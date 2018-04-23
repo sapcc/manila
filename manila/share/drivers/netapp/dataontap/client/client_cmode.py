8763c841726bd2b0e1352531140a926359082161
Maurice Schreiber <maurice.schreiber@sap.com>
2018-04-23 08:56:42 +0200

    cifs add AD security service server as preferred DC
    multiple comma separated entries can be given
    
    Change-Id: I9d6c79704d0ceb7a6fdc18035992e707327dd908

--- manila/share/drivers/netapp/dataontap/client/client_cmode.py
+++ manila/share/drivers/netapp/dataontap/client/client_cmode.py
@@ -1383,6 +1383,7 @@ class NetAppCmodeClient(client_base.NetAppBaseClient):
     def configure_active_directory(self, security_service, vserver_name):
         """Configures AD on Vserver."""
         self.configure_dns(security_service)
+        self.set_preferred_dc(security_service)
 
         # 'cifs-server' is CIFS Server NetBIOS Name, max length is 15.
         # Should be unique within each domain (data['domain'])
@@ -1482,6 +1483,26 @@ class NetAppCmodeClient(client_base.NetAppBaseClient):
             else:
                 msg = _("Failed to configure DNS. %s")
                 raise exception.NetAppException(msg % e.message)
+
+    @na_utils.trace
+    def set_preferred_dc(self, security_service):
+        # server is optional
+        if not security_service['server']:
+            return
+
+        api_args = {
+            'preferred-dc': [],
+            'domain': security_service['domain'],
+        }
+        dc_ips = [x.strip() for x in security_service['server'].split(',')]
+        for ip in dc_ips:
+            api_args['preferred-dc'].append({'string': ip})
+
+        try:
+            self.send_request('cifs-domain-preferred-dc-add', api_args)
+        except netapp_api.NaApiError as e:
+            msg = _("Failed to set preferred DC. %s")
+            raise exception.NetAppException(msg % e.message)
 
     @na_utils.trace
     def create_volume(self, aggregate_name, volume_name, size_gb,
