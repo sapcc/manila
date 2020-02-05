b5a6a5db80fd98fd5d9920c770935b39b4cc00d1
Maurice Escher <maurice.escher@sap.com>
2020-02-05 10:38:07 +0100

    ccloud: unused share servers can be transitioning
    The create/delete operation may have been interrupted by service stop.
    
    Change-Id: I4fecf91f5c927bc13838533227da8095ab03d315

--- manila/db/sqlalchemy/api.py
+++ manila/db/sqlalchemy/api.py
@@ -5070,6 +5070,8 @@ def share_server_get_all_unused_deletable(context, host, updated_before):
         constants.STATUS_INACTIVE,
         constants.STATUS_ACTIVE,
         constants.STATUS_ERROR,
+        constants.STATUS_CREATING,
+        constants.STATUS_DELETING,
     )
     result = (_server_get_query(context)
               .filter_by(is_auto_deletable=True)
