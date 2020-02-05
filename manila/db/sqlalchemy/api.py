c3feb447ba480cee819835e019f0b43aac2e7d5f
Maurice Escher <maurice.escher@sap.com>
2020-02-05 10:38:07 +0100

    ccloud: unused share servers can be stuck in creating
    Change-Id: I4fecf91f5c927bc13838533227da8095ab03d315

--- manila/db/sqlalchemy/api.py
+++ manila/db/sqlalchemy/api.py
@@ -3747,6 +3747,7 @@ def share_server_get_all_unused_deletable(context, host, updated_before):
         constants.STATUS_INACTIVE,
         constants.STATUS_ACTIVE,
         constants.STATUS_ERROR,
+        constants.STATUS_CREATING,
     )
     result = (_server_get_query(context)
               .filter_by(is_auto_deletable=True)
