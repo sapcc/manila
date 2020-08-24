37568d310952aa0249f45581f2105a57dbb951ed
Dmitry Galkin <galkindmitrii@gmail.com>
2020-08-24 10:19:47 +0200

    ccloud: added MULTI share protocol to export over NFS and CIFS
    at the same time
    
    - Add new protocol to supported and enabled by default.
    - Adjust NetApp driver helpers for licenses and exports.
    - Set UNIX mode to 777 for MULTI shares.
    - Handle access rules (both IP and USER) for MULTI shares.
    - fail early if ad and ldap security services are missing
    to create a multi protocol share
    - support storage protocol filter
    
    Change-Id: I748a1bb05bdcdb859c09fdc9ec729b63d8fda9e8
    Co-authored-by: Maurice Escher <maurice.escher@sap.com>
    Co-authored-by: Kiran Pawar <kiran.pawar@sap.com>
    (cherry picked from commit 527c73cf800133083a666bece047c267b4f336dd)

--- manila/share/drivers/netapp/dataontap/protocols/cifs_cmode.py
+++ manila/share/drivers/netapp/dataontap/protocols/cifs_cmode.py
@@ -55,8 +55,9 @@ class NetAppCmodeCIFSHelper(base.NetAppBaseHelper):
             self._client.create_cifs_share(share_name, export_path)
             self._client.remove_cifs_share_access(share_name, 'Everyone')
 
-        # Ensure 'ntfs' security style for RW volume. DP volumes cannot set it.
-        if not replica:
+        # Ensure 'ntfs' security style if not a MULTI share protocol.
+        # DP volumes cannot set it.
+        if (not replica) and (share['share_proto'].lower() != 'multi'):
             self._client.set_volume_security_style(share_name,
                                                    security_style='ntfs')
 
@@ -76,14 +77,21 @@ class NetAppCmodeCIFSHelper(base.NetAppBaseHelper):
     def update_access(self, share, share_name, rules):
         """Replaces the list of access rules known to the backend storage."""
 
-        _, cifs_share_name = self._get_export_location(share)
         # Ensure rules are valid
+        valid_rules = []
         for rule in rules:
-            self._validate_access_rule(rule)
+            if share['share_proto'].lower() == 'multi':
+                # multi-export share case, filter ip rules:
+                if rule['access_type'] == 'ip':
+                    continue
 
-        new_rules = {rule['access_to']: rule['access_level'] for rule in rules}
+            self._validate_access_rule(rule)
+            valid_rules.append(rule)
+
+        new_rules = {r['access_to']: r['access_level'] for r in valid_rules}
 
         # Get rules from share
+        _, cifs_share_name = self._get_export_location(share)
         existing_rules = self._get_access_rules(share, cifs_share_name)
 
         # Update rules in an order that will prevent transient disruptions
