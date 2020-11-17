8ddd691a06763d9260b0bfbacb0f2cc93f3d04ba
Maurice Escher <maurice.escher@sap.com>
2020-11-17 10:25:54 +0100

    NetApp encrypt option differentiate between not present and false
    If the encrypt extra spec is not set, now the setting is not treated
    as 'false', i.e. no encryption. But the parameter will not be touched
    to let the back end netapp operator decide. This allows for a mixed
    setup of encrypted and non encrypted back ends without leaking this
    detail up to the share type.
    
    Change-Id: Ie76721cae7f48d006b25ddf5266d48136a3f7f40

--- manila/share/drivers/netapp/dataontap/client/client_cmode.py
+++ manila/share/drivers/netapp/dataontap/client/client_cmode.py
@@ -2132,7 +2132,7 @@ class NetAppCmodeClient(client_base.NetAppBaseClient):
                       compression_enabled=False, max_files=None,
                       snapshot_reserve=None, volume_type='rw', comment='',
                       qos_policy_group=None, adaptive_qos_policy_group=None,
-                      encrypt=False, **options):
+                      encrypt=None, **options):
         """Creates a volume."""
         if adaptive_qos_policy_group and not self.features.ADAPTIVE_QOS:
             msg = 'Adaptive QoS not supported on this backend ONTAP version.'
@@ -2161,7 +2161,7 @@ class NetAppCmodeClient(client_base.NetAppBaseClient):
                             thin_provisioned=False, snapshot_policy=None,
                             language=None, snapshot_reserve=None,
                             volume_type='rw', comment='',
-                            qos_policy_group=None, encrypt=False,
+                            qos_policy_group=None, encrypt=None,
                             adaptive_qos_policy_group=None,
                             auto_provisioned=False, **options):
         """Creates a volume asynchronously."""
@@ -2217,12 +2217,14 @@ class NetAppCmodeClient(client_base.NetAppBaseClient):
             api_args['qos-adaptive-policy-group-name'] = (
                 adaptive_qos_policy_group)
 
-        if encrypt is True:
-            if not self.features.FLEXVOL_ENCRYPTION:
+        if encrypt is not None:
+            if encrypt is True and not self.features.FLEXVOL_ENCRYPTION:
                 msg = 'Flexvol encryption is not supported on this backend.'
                 raise exception.NetAppException(msg)
-            else:
+            elif encrypt is True:
                 api_args['encrypt'] = 'true'
+            else:
+                api_args['encrypt'] = 'false'
 
         return api_args
 
