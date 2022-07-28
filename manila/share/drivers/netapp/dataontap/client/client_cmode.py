83a82927843273874eed03287d681a4abfebfc34
Dmitry Galkin <galkindmitrii@gmail.com>
2022-07-28 14:46:24 +0200

    Allow disabling NFSv4.0
    NFS v4.0 was previously handled differently in NetApp driver.
    We want to explicitly disable v4.0 if it is not set in 'netapp_enabled_share_protocols'.
    
    not sending the optional 'is-nfsv40-enabled' parameter to
    'nfs-service-modify' means it will not be changed,
    which is relevant for update_vserver in the ensure loop.
    
    I.e. we don't want to disable nfs v4.0 for existing vservers.
    
    Also enable v4.1 options like read/write delegation, pnfs, acls.
    
    Change-Id: I1e2aa559cceede6505578bef38c1a5b2edf2d991

--- manila/share/drivers/netapp/dataontap/client/client_cmode.py
+++ manila/share/drivers/netapp/dataontap/client/client_cmode.py
@@ -1568,6 +1568,13 @@ class NetAppCmodeClient(client_base.NetAppBaseClient):
     def _enable_nfs_protocols(self, versions):
         """Set the enabled NFS protocol versions."""
         nfs3 = 'true' if 'nfs3' in versions else 'false'
+        # SAPCC absence of 'nfs4.0' should not equal 'false'
+        # instead it should mean:
+        #   don't touch existing value for existing vservers
+        #   and set the default value ('false' for ONTAP 9.7 and newer)
+        #   for new vservers
+        # Change back to upstream once all vservers have nfs4.0 disabled!
+        # nfs40 = 'true' if 'nfs4.0' in versions else 'false'
         nfs41 = 'true' if 'nfs4.1' in versions else 'false'
 
         nfs_service_modify_args = {
@@ -1582,6 +1589,15 @@ class NetAppCmodeClient(client_base.NetAppBaseClient):
 
         if 'nfs4.0' in versions:
             nfs_service_modify_args['is-nfsv40-enabled'] = 'true'
+        if 'nfs4.1' in versions:
+            nfs41_opts = {
+                'is-nfsv41-pnfs-enabled': 'true',
+                'is-nfsv41-acl-enabled': 'true',
+                'is-nfsv41-read-delegation-enabled': 'true',
+                'is-nfsv41-write-delegation-enabled': 'true',
+            }
+
+            nfs_service_modify_args.update(nfs41_opts)
 
         self.send_request('nfs-service-modify', nfs_service_modify_args)
 
