cf962b62933681b0c2ed238bb6cc8b6c4847fb3b
Maurice Escher <maurice.escher@sap.com>
2024-05-31 16:49:10 +0200

    sapcc: more attempts at cifs setup
    dc config validation may fail, because network connectivity is not
    given at that point of setup
    
    run another round with the different discovery modes: to keep current
    behaviour in the game
    (previously we only set those after cifs-server-create?)
    
    and also more attempts at cifs dns setup
    
    dns config validation may fail, because network connectivity is not
    given at that point of setup
    
    Change-Id: I4223aa24fc174583735cdd8dee01f91f07b53f6a
    (cherry picked from commit 9ebe528a33f33de61dff95d6ea3d78d5ec8a0e81)

--- manila/tests/share/drivers/netapp/dataontap/client/test_client_cmode.py
+++ manila/tests/share/drivers/netapp/dataontap/client/test_client_cmode.py
@@ -2479,7 +2479,6 @@ class NetAppClientCmodeTestCase(test.TestCase):
 
         self.mock_object(self.client, 'send_request')
         self.mock_object(self.vserver_client, 'configure_active_directory')
-        self.mock_object(self.vserver_client, 'configure_cifs_options')
 
         self.client.setup_security_services([fake.CIFS_SECURITY_SERVICE],
                                             self.vserver_client,
@@ -2501,8 +2500,6 @@ class NetAppClientCmodeTestCase(test.TestCase):
             mock.call('vserver-modify', vserver_modify_args)])
         self.vserver_client.configure_active_directory.assert_has_calls([
             mock.call(fake.CIFS_SECURITY_SERVICE, fake.VSERVER_NAME, False)])
-        self.vserver_client.configure_cifs_options.assert_has_calls([
-            mock.call(fake.CIFS_SECURITY_SERVICE)])
 
     def test_setup_security_services_kerberos(self):
 
