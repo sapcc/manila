9c55a95efb7b6f1bb6cea02f034597f2159f6e6b
Kiran Pawar <kinpaa@gmail.com>
2024-09-12 13:47:26 +0000

    Pass share network subnet metadata updates to backend drivers
    - Share network subnet metadata if updated and metadata keys are present
    in config option 'driver_updatable_subnet_metadata', then metadata will
    be passed to backend driver. Driver then can take action and the result
    will be communicated in user message. For delete operation, if metadata
    updatable by driver exist with resource, user message will be added and
    delete will be performed.
    - Supported from microversion '2.81'. Modified backport for 2023.1-m3
    
    Implements: blueprint pass-resource-metadata-updates-to-backend-drivers
    Change-Id: I2f81fed925cfcd1ac8b99472cd836c2b339ee487
    (cherry picked from commit eb131b92d80c2f4de114c2323d8f2d9120a07ee8)
    (cherry picked from commit 9f7d4d66e764ef3ef919f16bf1ca9717a9330719)

--- manila/message/message_field.py
+++ manila/message/message_field.py
@@ -21,6 +21,7 @@ class Resource(object):
     SHARE_REPLICA = 'SHARE_REPLICA'
     SHARE_SNAPSHOT = 'SHARE_SNAPSHOT'
     SECURITY_SERVICE = 'SECURITY_SERVICE'
+    SHARE_NETWORK_SUBNET = 'SHARE_NETWORK_SUBNET'
 
 
 class Action(object):
@@ -159,6 +160,10 @@ class Detail(object):
         '030',
         _("Metadata passed to share driver failed to perform required "
           "operation."))
+    UPDATE_METADATA_NOT_DELETED = (
+        '031',
+        _("Metadata delete operation includes driver updatable metadata, and "
+          "it is not passed to share driver to perform required operation."))
 
     ALL = (
         UNKNOWN_ERROR,
@@ -192,6 +197,7 @@ class Detail(object):
         DRIVER_FAILED_TRANSFER_ACCEPT,
         UPDATE_METADATA_SUCCESS,
         UPDATE_METADATA_FAILURE,
+        UPDATE_METADATA_NOT_DELETED
     )
 
     # Exception and detail mappings
