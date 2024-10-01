f0a6e79f77619866d5ebb10215f5abe85f23f98f
Kiran Pawar <kinpaa@gmail.com>
2024-10-01 14:54:54 +0000

    Allow to update access level of access rule (2.81)
    Update API will only allow to update access_level of access_type.
    Pass the information to share drivers and let them handle update
    access rule if supported.
    
    Closes-bug: #2066871
    Change-Id: Ieb836d7e63ce8bb14c4ae4fc57b012ee3249dcc7
    (cherry picked from commit 65d48c0589379642c9219eac7709199420fb7aac)

--- manila/share/drivers/cephfs/driver.py
+++ manila/share/drivers/cephfs/driver.py
@@ -535,7 +535,7 @@ class CephFSDriver(driver.ExecuteMixin, driver.GaneshaMixin,
         rados_command(self.rados_client, "fs subvolume rm", argdict)
 
     def update_access(self, context, share, access_rules, add_rules,
-                      delete_rules, share_server=None):
+                      delete_rules, update_rules, share_server=None):
         return self.protocol_helper.update_access(
             context, share, access_rules, add_rules, delete_rules,
             share_server=share_server)
@@ -895,7 +895,7 @@ class NativeProtocolHelper(ganesha.NASHelperBase):
         rados_command(self.rados_client, "fs subvolume evict", argdict)
 
     def update_access(self, context, share, access_rules, add_rules,
-                      delete_rules, share_server=None):
+                      delete_rules, update_rules, share_server=None):
         access_updates = {}
 
         argdict = {
@@ -1260,7 +1260,7 @@ class NFSClusterProtocolHelper(NFSProtocolHelperMixin, ganesha.NASHelperBase):
         rados_command(self.rados_client, "nfs export rm", argdict)
 
     def update_access(self, context, share, access_rules, add_rules,
-                      delete_rules, share_server=None):
+                      delete_rules, update_rules, share_server=None):
         """Update access rules of share.
 
         Creates an export per share. Modifies access rules of shares by
