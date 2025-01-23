137c42275c7c8e2e20283e96d482baa64f47456d
Ganesh Kugulakrishnan <ganesh.kugulakrishnan@gmail.com>
2025-01-22 18:06:38 -0600

    [gh-issue#80] Added exception block to display message in Dashboard UI

--- manila/share/manager.py
+++ manila/share/manager.py
@@ -2212,6 +2212,22 @@ class ShareManager(manager.SchedulerDependentManager):
                         resource_id=share_id,
                         detail=(message_field.Detail
                                 .SECURITY_SERVICE_FAILED_AUTH))
+            except exception.IpAddressGenerationFailureClient:
+                with excutils.save_and_reraise_exception():
+                    error = ("Creation of share instance %s failed: "
+                             "No Free IP's in neutron subnet.")
+                    LOG.warning(error, share_instance_id)
+                    self.db.share_instance_update(
+                        context, share_instance_id,
+                        {'status': constants.STATUS_ERROR}
+                    )
+                    self.message_api.create(
+                        context,
+                        message_field.Action.CREATE,
+                        share['project_id'],
+                        resource_type=message_field.Resource.SHARE,
+                        resource_id=share_id,
+                        detail=message_field.Detail.NEUTRON_SUBNET_CAPACITY_REACHED)
             except Exception:
                 with excutils.save_and_reraise_exception():
                     error = ("Creation of share instance %s failed: "
