5621249f349268ae3cbba2dcfeb5d548d5272484
Ganesh Kugulakrishnan <97788209+RockSolidScripts@users.noreply.github.com>
2025-01-27 10:27:39 -0600

    [gh-issue#80] Neuton Subnet Full Exception Handling (#237)
    * [gh-issue#80] Neuton Subnet Full Exception Handling
    
    * [gh-issue#80] added subnet ID in the error message
    
    * [gh-issue#80] Added exception block to display message in Dashboard UI
    
    * Update manila/message/message_field.py
    
    Co-authored-by: Maurice Escher <maurice.escher@sap.com>
    
    * [gh-issue#80] Fixed pep8 checks
    
    * [gh-issue#80] Fixed pep8 checks #2
    
    * [gh-issue#80] Fixed pep8 checks #3
    
    * [gh-issue#80] Fixed pep8 checks #4
    
    * [gh-issue#80] Fixed pep8 checks #5
    
    ---------
    
    Co-authored-by: Ganehs Kugulakrishnan <ganesh.kugulakrishnan@sap.com>
    Co-authored-by: Maurice Escher <maurice.escher@sap.com>

--- manila/message/message_field.py
+++ manila/message/message_field.py
@@ -150,7 +150,14 @@ class Detail(object):
           "due to port limit exceeded. You may increase quota of network "
           "ports and retry your request. If this doesn't work, contact your "
           "administrator to troubleshoot issues with your network."))
-
+    NEUTRON_SUBNET_FULL = (
+        '030',
+        _("Share Driver failed to create share server on share network "
+          "due no more free IP addresses in the neutron subnet."
+          "You may free some IP addresses in the subnet "
+          "or create a new subnet/share network. If this doesn't work, "
+          "contact your administrator to troubleshoot "
+          "issues with your network."))
     ALL = (
         UNKNOWN_ERROR,
         NO_VALID_HOST,
@@ -180,7 +187,8 @@ class Detail(object):
         DRIVER_FAILED_DELETE_SHARE_SNAPMIRROR,
         FILTER_AFFINITY,
         FILTER_ANTI_AFFINITY,
-        SHARE_NETWORK_PORT_QUOTA_LIMIT_EXCEEDED
+        SHARE_NETWORK_PORT_QUOTA_LIMIT_EXCEEDED,
+        NEUTRON_SUBNET_FULL
     )
 
     # Exception and detail mappings
