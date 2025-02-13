d80d680cfff454fb3f271cbef725b79788317314
Maurice Escher <maurice.escher@sap.com>
2025-02-14 00:46:27 +0100

    NetApp disable nfs v4.0 for new share servers
    this has been the default for ONTAP 9.7 and 9.8 and we mistakenly
    relied on that it would stay that way in
    3503a199a00cef176c398b98a4187808f5d521ad
    
    But since ONTAP 9.9.1 the default is to enable nfsv40 again.
    
    Now making the disablement explicit, since we now have a proper way to
    distinguish between options that should be set on create and those
    that should be set on update, too.
    
    Change-Id: I39e2da2c3393ec95312c09a608a997a29981a471

--- manila/tests/share/drivers/netapp/dataontap/client/test_client_cmode.py
+++ manila/tests/share/drivers/netapp/dataontap/client/test_client_cmode.py
@@ -2656,17 +2656,12 @@ class NetAppClientCmodeTestCase(test.TestCase):
         self.client._enable_nfs_protocols(versions, method, extra)
 
         nfs_service_modify_args = {
-            'is-nfsv3-enabled': 'true' if v3 else 'false',
-            'is-nfsv41-enabled': 'true' if v41 else 'false',
-            'showmount': 'true',
             'is-v3-ms-dos-client-enabled': 'true',
             'is-nfsv3-connection-drop-enabled': 'false',
             'enable-ejukebox': 'false',
             'is-vstorage-enabled': 'true',
         }
 
-        if v40:
-            nfs_service_modify_args['is-nfsv40-enabled'] = 'true'
         if v41:
             nfs41_opts = {
                 'is-nfsv41-acl-enabled': 'false',
@@ -2678,10 +2673,16 @@ class NetAppClientCmodeTestCase(test.TestCase):
 
         if method == 'create':
             flexgroup_opts = {
+                'is-nfsv3-enabled': 'true' if v3 else 'false',
+                'is-nfsv40-enabled': 'true' if v40 else 'false',
+                'is-nfsv41-enabled': 'true' if v41 else 'false',
+                'showmount': 'true',
                 'is-nfsv3-64bit-identifiers-enabled': 'true',
                 'is-nfsv4-64bit-identifiers-enabled': 'true',
             }
             nfs_service_modify_args.update(flexgroup_opts)
+            if v41:
+                nfs_service_modify_args['is-nfsv41-pnfs-enabled'] = 'false'
 
         if extra:
             nfs_service_modify_args.update(extra)
