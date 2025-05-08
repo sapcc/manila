9b3b3949c85e78ddceec6301b0db5697ec09fd08
Kiran Pawar <kiran.pawar@sap.com>
2025-05-08 13:24:26 +0000

    Consider share metadata snapshot_policy
    The snapshot_policy if set in share metadata would be given higher
    preference over share type extra-spec provided snapshot_policy. Also
    this will override the snapshot_policy exceptions.
    
    Change-Id: I7731cb7a32e0bd5418a8158350d56eefa69a5db4

--- manila/share/manager.py
+++ manila/share/manager.py
@@ -573,6 +573,8 @@ class ShareManager(manager.SchedulerDependentManager):
                 continue
             share_instance_dict = self._get_share_instance_dict(
                 ctxt, share_instance)
+            if metadata:
+                share_instance_dict.update({'metadata': metadata})
             update_share_instances.append(share_instance_dict)
 
         if update_share_instances:
