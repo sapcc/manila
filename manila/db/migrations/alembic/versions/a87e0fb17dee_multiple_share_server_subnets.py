2d7459c0dd8330613d42b2a29644f20094d72dbd
Maurice Escher <maurice.escher@sap.com>
2025-06-30 23:53:27 +0200

    add guard to multiple subnet migration
    we had share_server_id entries in the network_allocations table,
    where the corresponding share server was no longer existing in the
    share servers table.
    
    Change-Id: Icabbba34d5160e5f40a696dc2abd48b7478d7d7a
    (cherry picked from commit aec38b3c480f2c7e3e3e0614728926fbc4baa546)

--- manila/db/migrations/alembic/versions/a87e0fb17dee_multiple_share_server_subnets.py
+++ manila/db/migrations/alembic/versions/a87e0fb17dee_multiple_share_server_subnets.py
@@ -119,6 +119,9 @@ def upgrade():
                 alloc._mapping['share_server_id'] == (
                     share_servers_table.c.id))).first()
 
+        if not server:
+            continue
+
         # pylint: disable=no-value-for-parameter
         op.execute(network_allocation_table.update().where(
             alloc._mapping['id'] == network_allocation_table.c.id).values(
